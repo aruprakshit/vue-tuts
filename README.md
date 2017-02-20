184af42a24baee37bbf47e2ddbfe347aa6ea0ac7
Arup Rakshit <aruprakshit@rocketmail.com>
2017-02-20 12:49:21 +0530

    init toy vue app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vue.js and Webpack block https://skyronic.com/blog/vue-project-scratch
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  // This is the "main" file which should include all other modules
+  entry: './src/index.js',
+  // Where should the compiled file go?
+  output: {
+    // To the `dist` folder
+    path: path.resolve(__dirname, 'dist'),
+    // With the filename `build.js` so it's dist/build.js
+    filename: 'build.js'
+  },
+  module: {
+    // Special compilation rules
+    loaders: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: /\.js$/,
+        // Transform it with babel
+        loader: 'babel-loader',
+        // don't transform node_modules folder (which don't need to be compiled)
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  }
+}
